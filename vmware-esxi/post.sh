53587d208a77b6fcb17d5aacaecf636a852cd625
Alexsander de Souza <61709370+alexsander-souza@users.noreply.github.com>
2023-09-14 11:59:00 -0300

    improve ESXi build (#139)
    make fusefs handling more robust. review documentation
    
    ---------
    
    Co-authored-by: craig bender <19372989+ThinGuy@users.noreply.github.com>

--- vmware-esxi/post.sh
+++ vmware-esxi/post.sh
@@ -1,12 +1,13 @@
-#!/bin/sh
+#!/bin/sh -x
 
 echo 'Adding curtin-hooks to image...'
-mount_part 1 $TMP_DIR/boot fusefat
-cp -rv curtin $TMP_DIR/boot/
+mount_part 1 "$TMP_DIR"/boot fusefat
+cp -rv curtin "$TMP_DIR"/boot/
 
 echo 'Adding post-install scripts to image...'
-cp -v scripts.tar.xz $TMP_DIR/boot/curtin/
+cp -v scripts.tar.xz "$TMP_DIR"/boot/curtin/
 
 echo 'Unmounting image...'
-sync -f $TMP_DIR/boot
-fusermount -u $TMP_DIR/boot
+sync -f "$TMP_DIR"/boot
+fusermount -z -u "$TMP_DIR"/boot
+grep -qs "$TMP_DIR/boot " /proc/mounts && umount -f "$TMP_DIR"/boot
