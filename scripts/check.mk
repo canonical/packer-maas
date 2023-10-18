f0be59465eecf4f4a0a7143aef446c2a75f54d44
Jack Lloyd-Walters <jack.lloyd-walters@canonical.com>
2023-10-18 15:51:41 +0100

    switch out to explicit command for printing

--- scripts/check.mk
+++ scripts/check.mk
@@ -1,7 +1,18 @@
 define check_packages_deps
+_current_deps := libnbd-bin nbdkit packer fuse2fs $(1)
+_focal_deps := libnbd0 nbdkit packer fuse2fs $(2)
+
+print-deps:
 	@if [ $(shell lsb_release -sr|cut -d. -f1) -ge 22 ];then \
-		dpkg -s libnbd-bin nbdkit packer fuse2fs $(1); \
+		echo $$(_current_deps); \
 	else \
-		dpkg -s libnbd0 nbdkit packer fuse2fs $(2); \
+		echo $$(_focal_deps); \
+	fi
+
+check-deps:
+	@if [ $(shell lsb_release -sr|cut -d. -f1) -ge 22 ];then \
+		dpkg -s $$(_current_deps) > /dev/null; \
+	else \
+		dpkg -s $$(_focal_deps) > /dev/null; \
 	fi
 endef
